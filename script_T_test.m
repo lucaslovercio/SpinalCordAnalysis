883efb096e45cb785d77682f71a7095bfdb0fb30
Lucas D Lo Vercio <lucaslovercio@gmail.com>
2024-02-22 17:39:46 -0700

    labels in histogram

--- script_T_test.m
+++ script_T_test.m
@@ -33,15 +33,13 @@ x_limit_upper = max([max(sample_1),max(sample_2)]);
 [h,p] = ttest2(sample_1,sample_2)
 
 figure('Name','Histograms');
-
-%histf(sample_1,'facecolor',map(1,:),'facealpha',.5,'edgecolor','none')
 histogram(sample_1,15,'facealpha',.5,'edgecolor','none')
-title(column_names{1});
+column_name = strrep(column_names{1},'_',' ');
+title(column_name);
 hold on
 histogram(sample_2,15,'facealpha',.5,'edgecolor','none')
 hold off;
-%box off
+xlabel(column_name);
+ylabel('Frequency');
 axis([x_limit_lower x_limit_upper 0 100]);
-%legalpha('H1','H2','H3','location','northwest')
-%legend boxoff
 
